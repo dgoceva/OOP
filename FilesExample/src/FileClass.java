a1950b31ad058f7fc2792d16973ef16c6657f085
Yavor Tomov <yavor_tomov@tu-sofia.bg>
2020-11-05 10:48:08 +0200

    exchange words started

--- FilesExample/src/FileClass.java
+++ FilesExample/src/FileClass.java
@@ -3,41 +3,49 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
-
 public class FileClass {
-	private ArrayList<String> data;
+	private ArrayList<ArrayList<String>> data;
+
 	public FileClass() {
 		data = new ArrayList<>();
 	}
+
 	public FileClass(String fileName) {
 		this();
 		try {
-			Scanner sc = new Scanner(new File(fileName),"UTF-8");
+			Scanner sc = new Scanner(new File(fileName), "UTF-8");
 			while (sc.hasNextLine()) {
-				data.add(sc.nextLine());
+				String temp = sc.nextLine();
+				ArrayList<String> words = new ArrayList<>();
+				words.addAll(Arrays.asList(temp.split(" \t")));
+				data.add(words);
 			}
+			sc.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
 	public void exchangeRows(int x, int y) {
-		if (x<1 || x>data.size()) {
+		if (x < 1 || x > data.size()) {
 			System.out.println("x is not valid row number");
 			return;
 		}
-		if (y<1 || y>data.size()) {
+		if (y < 1 || y > data.size()) {
 			System.out.println("y is not valid row number");
 			return;
 		}
-		int start = Math.min(x-1, y-1);
-		int end = Math.max(x-1, y-1);
-		String temp = data.get(start);
-		data.set(start, data.get(end));
-		data.set(end, temp);
+		int start = Math.min(x - 1, y - 1);
+		int end = Math.max(x - 1, y - 1);
+//		String temp = data.get(start);
+//		data.set(start, data.get(end));
+//		data.set(end, temp);
 	}
+
 	public void writeToFile(String fileName) {
 		try {
 			FileWriter out = new FileWriter(fileName);
@@ -48,12 +56,15 @@ public class FileClass {
 			e.printStackTrace();
 		}
 	}
+
 	@Override
 	public String toString() {
 		StringBuilder str = new StringBuilder();
 
-		for (String s: data) {
-			str.append(s+"\n");
+		for (ArrayList<String> element : data) {
+			for (String s : element) {
+				str.append(s + "\n");
+			}
 		}
 		return str.toString();
 	}
--- FilesExample/src/FileExampleMain.java
+++ FilesExample/src/FileExampleMain.java
@@ -4,16 +4,16 @@ public class FileExampleMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		FileClass fc = new FileClass("D:\\UKTC\\FilesExample\\Problem.txt");
+		FileClass fc = new FileClass("Problem.txt");
 		System.out.println(fc);
-		Scanner sc = new Scanner(System.in);
-		System.out.print("x=");
-		int x = sc.nextInt();
-		System.out.print("y=");
-		int y = sc.nextInt();
-		fc.exchangeRows(x, y);
-		System.out.println(fc);
-		fc.writeToFile("Problem.out.txt");
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("x=");
+//		int x = sc.nextInt();
+//		System.out.print("y=");
+//		int y = sc.nextInt();
+//		fc.exchangeRows(x, y);
+//		System.out.println(fc);
+//		fc.writeToFile("Problem.out.txt");
 	}
 
 }
